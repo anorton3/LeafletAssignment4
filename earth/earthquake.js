dc7fd50f3037e775aad8e3779eb52587564a1e38
anorton3 <anorton3@uwyo.edu>
2024-10-07 09:39:00 -0600

    popup change

--- earth/earthquake.js
+++ earth/earthquake.js
@@ -27,15 +27,13 @@ $.getJSON(earthurl, function(data) {
             else if (feature.properties.mag === 1 ) alertColor = 'pink'
             return { color: alertColor };
         },
-        style : function(feature){
-            var point = 'earthquake';
-                if(feature.properties.type === 'earthquake') point = 'circle'
-                return {point: point}
         
         onEachFeature: function (feature, layer) {  
             layer.bindPopup(feature.properties.title);
+            layer.bindPopup(feature.properties.mag);
+            layer.bindPopup(feature.properties.time);
+
         }
-    }
     }).addTo(map);
 });
 
