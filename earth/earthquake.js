c05960c32d45f1dc21659b92b7c61e947e0d4ed1
anorton3 <anorton3@uwyo.edu>
2024-10-07 09:48:32 -0600

    hopefully fixed

--- earth/earthquake.js
+++ earth/earthquake.js
@@ -1,4 +1,4 @@
-var map = L.map('weathermap').setView([38, -95], 4);
+var map = L.map('earthquakemap').setView([38, -95], 4);
 
 var CartoDB_DarkMatterNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
@@ -16,9 +16,9 @@ var radarDisplayOptions = {
 
 var radar = L.tileLayer.wms(radarURL, radarDisplayOptions).addTo(map);
 
-var weatherAlertsUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+var earthurl= 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 
-$.getJSON(weatherAlertsUrl, function(data) {
+$.getJSON(earthurl, function(data) {
     L.geoJSON(data, {
         style: function(feature) {
             var alertColor = 'orange';
